adead95250e0339dc667b78e7356e58681c05daa
shin5ok <shin5ok@55mp.com>
2023-02-06 23:56:37 +0900

    set number of cpu and amount of memory while deploying app

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,4 @@
-gcloud run deploy --source=. --set-env-vars=PROJECT_ID=$PROJECT_ID,INSTANCE_NAME=test-instance,DATABASE_NAME=musics --service-account=$SA --region=us-central1 gorm-test 
+gcloud run deploy --source=. \
+--service-account=$SA --region=us-central1 gorm-test \
+--set-env-vars=PROJECT_ID=$PROJECT_ID,INSTANCE_NAME=test-instance,DATABASE_NAME=musics \
+--cpu=2 --memory=1Gi
