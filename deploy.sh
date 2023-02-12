66ad54483e303fc1bb530a5dd0ae41936caabf6f
shin5ok <shin5ok@55mp.com>
2023-02-13 01:14:54 +0900

    modify it

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,6 @@
+CONNECTION_STRING="host=localhost port=5432"
 gcloud run deploy --source=. \
---service-account=$SA --region=us-central1 gorm-test \
---set-env-vars=PROJECT_ID=$PROJECT_ID,INSTANCE_NAME=test-instance,DATABASE_NAME=musics \
---cpu=2 --memory=1Gi
+--service-account=$SA gorm-test \
+--set-env-vars=PROJECT_ID=$GOOGLE_CLOUD_PROJECT,INSTANCE_NAME=test-instance,DATABASE_NAME=music,CONNECTION_STRING="$CONNECTION_STRING" \
+--region=us-east5 \
+--cpu=1 --memory=2Gi
